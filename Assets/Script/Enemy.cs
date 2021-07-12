6fb397ab381ee12eb108c5ac83eadb18ace7a25f
MarshFfeco <anderson22khg@gmail.com>
2021-07-12 14:03:17 -0300

    Finished
    Ready for testing.

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float playerZ;
+
+    private Vector3 offset;
+
+
+    private void Update()
+    {
+        float playerZ = GameObject.Find("Player").transform.position.z - 4;
+        float Enemy = transform.position.z;
+
+        if (playerZ > Enemy)
+        {
+            PlayerController pCon = GameObject.Find("Player").GetComponent<PlayerController>();
+            pCon.point = pCon.point + 1;
+            pCon.pontUI.GetComponent<UnityEngine.UI.Text>().text = pCon.point.ToString();
+
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public GameObject cameraMov;
+    private GameObject gmUI;
+    public GameObject pontUI;
+
+    public float speed = 10f;
+    private float moveX;
+
+    private Vector3 offset;
+
+    private bool gm;
+    public bool canJump = true;
+
+    public int point;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+        gmUI = GameObject.Find("Canvas");
+        pontUI = GameObject.Find("Canvas2").transform.Find("Text").gameObject;
+
+        gmUI.SetActive(false);
+        gm = false;
+
+        offset = cameraMov.transform.position - transform.position;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (gm == false)
+        {
+
+            rb.AddForce(speed * Vector3.forward);
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(speed * Vector3.left);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(speed * Vector3.right);
+            }
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                cameraMov.transform.Rotate(0, -1f, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                cameraMov.transform.Rotate(0, 1, 0);
+            }
+            if (canJump && Input.GetKey(KeyCode.Space))
+            {
+                rb.velocity = new Vector3(0, 10, 0);
+                canJump = false;
+            }
+
+        } else if(gm == true)
+        {
+            Time.timeScale = 0;
+            Time.timeScale = 1;
+
+        }
+
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        cameraMov.transform.position = transform.position + offset;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            gmUI.SetActive(true);
+            gm = true;
+        }
+
+        if(other.gameObject.tag == "Floor")
+        {
+            canJump= true;
+        }
+    }
+}
